a1e9698ed7f9d34387bdf6a1487909537b485907
Mario Dot Net <contactomariodotnet@gmail.com>
2017-04-24 00:11:24 -0400

    xx
    xx

--- DotNetCore/ConsoleAppDotNetCoreDemo/ConsoleAppDotNetCoreDemo/Program.cs
+++ DotNetCore/ConsoleAppDotNetCoreDemo/ConsoleAppDotNetCoreDemo/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppDotNetCoreDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
