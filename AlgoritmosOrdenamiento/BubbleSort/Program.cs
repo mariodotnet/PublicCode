523cb8aacc2272a73f1b20b9dcbb3bcbd5ed010f
Mario Dot Net <contactomariodotnet@gmail.com>
2017-04-27 00:25:41 -0400

    test

---
+++ AlgoritmosOrdenamiento/BubbleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input array
+            int[] input = { 1, 5, 3, 2, 4 };
+
+            int temp = 0;
+
+            for (int write = 0; write < input.Length; write++)
+            {
+                for (int sort = 0; sort < input.Length - 1; sort++)
+                {
+                    if (input[sort] > input[sort + 1])
+                    {
+                        temp = input[sort + 1];
+                        input[sort + 1] = input[sort];
+                        input[sort] = temp;
+                    }
+                }
+            }
+
+            // print array
+            for (int i = 0; i < input.Length; i++)
+                Console.Write(input[i] + " ");
+
+            // screen pause
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AlgoritmosOrdenamiento/CocktailSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 3, 5, 2, 4, 6 };
+
+            var result = cocktailSort(input);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Sorts input array.
+        /// </summary>
+        /// <returns></returns>
+        public static int[] cocktailSort(int[] input)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i <= input.Length - 2; i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        //test whether the two elements are in the wrong order
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    //we can exit the outer loop here if no swaps occurred.
+                    break;
+                }
+                swapped = false;
+                for (int i = input.Length - 2; i >= 0; i--)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                //if no elements have been swapped, then the list is sorted
+            } while (swapped);
+
+            return input;
+        }
+    }
+}
