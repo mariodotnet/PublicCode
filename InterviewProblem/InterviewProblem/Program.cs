741c2c692102536aa4d01dc2465c4db21f844225
Mario Dot Net <contactomariodotnet@gmail.com>
2017-06-07 22:24:02 -0400

    p

---
+++ InterviewProblem/InterviewProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //incomplete....
+            Console.WriteLine(GetNumber());
+            Console.ReadLine();
+        }
+
+        public static int GetNumber()
+        {
+            Random ran = new Random();
+            int count = 1;
+            int prev = -1;
+            string num = "";
+            do
+            {
+                int r = 0;
+                while (r % 2 == 0 || r == prev)
+                {
+                    r = ran.Next(0, 9);
+                }
+
+                num += r.ToString();
+                count++;
+
+            } while (count <= 4);
+
+            return int.Parse(num);
+        }
+    }
+}
--- WatchOSApp1/WatchOSApp1/WatchOSApp1.WatchOSApp/Properties/AssemblyInfo.cs
+++ WatchOSApp1/WatchOSApp1/WatchOSApp1.WatchOSApp/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("WatchOSApp1.WatchOSApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("WatchOSApp1.WatchOSApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9941b20d-d97c-40d0-b8a2-f98de07c6012")]
+[assembly: Guid("a5e9e401-e5a1-4ea5-b0f9-d927f3285aea")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
