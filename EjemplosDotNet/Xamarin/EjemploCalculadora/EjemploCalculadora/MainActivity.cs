1c4c476972fb1f4d0a5998326bea28ce77ee6e8e
mariodotnet <contactomariodotnet@gmail.com>
2017-11-05 22:56:49 -0500

    subiendo avance

--- EjemplosDotNet/Xamarin/EjemploCalculadora/EjemploCalculadora/MainActivity.cs
+++ EjemplosDotNet/Xamarin/EjemploCalculadora/EjemploCalculadora/MainActivity.cs
@@ -13,7 +13,23 @@ namespace EjemploCalculadora
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            //btn1 click, suma
+            Button b1 = FindViewById<Button>(Resource.Id.button1);
+            b1.Click += B1_Click;
+        }
+
+        //Handles click
+        private void B1_Click(object sender, System.EventArgs e)
+        {
+            EditText input1 = FindViewById<EditText>(Resource.Id.editText1);
+            EditText input2 = FindViewById<EditText>(Resource.Id.editText2);
+            TextView r = FindViewById<TextView>(Resource.Id.textView3);
+
+            r.Text = (int.Parse(input1.Text) + int.Parse(input2.Text)).ToString();
         }
     }
+
+
 }
 
--- EjemplosDotNet/Xamarin/EjemploCalculadora/EjemploCalculadora/Resources/Resource.Designer.cs
+++ EjemplosDotNet/Xamarin/EjemploCalculadora/EjemploCalculadora/Resources/Resource.Designer.cs
@@ -47,6 +47,24 @@ namespace EjemploCalculadora
 			// aapt resource value: 0x7f040000
 			public const int button1 = 2130968576;
 			
+			// aapt resource value: 0x7f040002
+			public const int button4 = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int button5 = 2130968579;
+			
+			// aapt resource value: 0x7f040001
+			public const int button6 = 2130968577;
+			
+			// aapt resource value: 0x7f040004
+			public const int editText1 = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int editText2 = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int textView3 = 2130968582;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
