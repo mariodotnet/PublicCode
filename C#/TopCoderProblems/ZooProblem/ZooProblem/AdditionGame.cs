76dd414989876806c5a039eb2eaddaf6f87ea1ed
mariodotnet <contactomariodotnet@gmail.com>
2017-11-10 02:17:14 -0500

    no se que paso pero quiere checkin

---
+++ C#/TopCoderProblems/ZooProblem/ZooProblem/AdditionGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooProblem
+{
+    public class AdditionGame
+    {
+        public int getMaximumPoints(int A, int B, int C, int N)
+        {
+            int points = 0;
+            while (N > 0 && (A > 0 || B >= 0 || C >= 0))
+            {
+                if (A >= B && A >= C)
+                {
+                    points += A;
+                    A--;
+                }
+                else if (B >= A && B >= C)
+                {
+                    points += B;
+                    B--;
+                }
+                else if (C >= B && C >= A)
+                {
+                    points += C;
+                    C--;
+                }
+
+                N--;
+            }
+
+            return points;
+        }
+    }
+}
---
+++ C#/TopCoderProblems/ZooProblem/ZooProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new AdditionGame();
+
+            int[] i = new int[4];
+            i[0] = 3;
+            i[1] = 4;
+            i[2] = 5;
+            i[3] = 3;
+
+            var result = c.getMaximumPoints(i[0], i[1], i[2], i[3]);
+        }
+    }
+}
