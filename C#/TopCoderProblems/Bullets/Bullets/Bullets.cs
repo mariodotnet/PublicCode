4d4b101f1d0a63945cd8ba20271141106b8455fe
mariodotnet <contactomariodotnet@gmail.com>
2017-11-11 17:21:23 -0500

    solución problema bullets

---
+++ C#/TopCoderProblems/Bullets/Bullets/Bullets.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullets
+{
+    class Bullets
+    {
+        public int match(string[] guns, string bullet)
+        {
+
+            for (int x = 0; x < guns.Length; x++)
+            {
+                if (guns[x] == bullet) return x;
+
+                if (guns[x] != null)
+                {
+                    string tmp = swifth(guns[x]);
+                    for (int i = 0; i < guns[x].Length; i++)
+                    {
+                        tmp = swifth(tmp);
+                        if (tmp == bullet)
+                            return x;
+                    }
+                }
+
+            }
+
+            return -1;
+        }
+
+        private string swifth(string v)
+        {
+            if (v.Length > 1)
+            {
+                string s1 = v.Substring(v.Length - 1, 1);
+                string s2 = v.Substring(0, v.Length - 1);
+                return s1 + s2;
+
+            }
+            return v;
+        }
+    }
+}
---
+++ C#/TopCoderProblems/Bullets/Bullets/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bullets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var b = new Bullets();
+
+            var x = new string[2];
+
+            x[0] = "|| || ||";
+            x[1] = "||| ||| ";
+
+            var r = b.match(x, "|| ||| |");
+        }
+    }
+}
