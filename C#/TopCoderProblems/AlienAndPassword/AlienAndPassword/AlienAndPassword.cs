9a324c5a573bf025f361534ccc2997f83981d8fc
mariodotnet <contactomariodotnet@gmail.com>
2017-11-13 01:33:52 -0500

    solucion problema topcoder

---
+++ C#/TopCoderProblems/AlienAndPassword/AlienAndPassword/AlienAndPassword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienAndPassword
+{
+        class AlienAndPassword
+        {
+            public int getNumber(string S)
+            {
+                List<string> l = new List<string>();
+
+                for (int i = 0; i < S.Length; i++)
+                {
+                    var arr = S.ToList();
+                    arr.RemoveAt(i);
+                    l.Add(string.Concat(arr));
+                }
+
+                l = l.Distinct().ToList();
+
+                return l.Count;
+            }
+        }
+}
