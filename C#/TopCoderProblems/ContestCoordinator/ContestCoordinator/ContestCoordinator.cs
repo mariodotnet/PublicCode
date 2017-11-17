37485aa0414d83bd220562bdd8c443e0d84676d4
mariodotnet <contactomariodotnet@gmail.com>
2017-11-17 00:01:11 -0500

    topcoder, solución

---
+++ C#/TopCoderProblems/ContestCoordinator/ContestCoordinator/ContestCoordinator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestCoordinator
+{
+    class ContestCoordinator
+    {
+        public double bestAverage(int[] scores)
+        {
+            List<int> s = scores.ToList();
+
+            s.Sort();
+
+            if (s.Distinct().ToList().Count == 1)
+            {
+                return s.First();
+            }
+
+            var tempValue = s.First();
+            var tempValue2 = s.Last();
+
+            bool removed = true;
+            bool removed2 = true;
+            while (removed || removed2)
+            {
+                removed = s.Remove(tempValue);
+                removed2 = s.Remove(tempValue2);
+            }
+
+
+            return s.Average();
+        }
+    }
+}
---
+++ C#/TopCoderProblems/ContestCoordinator/ContestCoordinator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestCoordinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new ContestCoordinator();
+
+            int[] input = { 1, 13, 8, 6, 7, 9 };
+
+            var r = o.bestAverage(input);
+        }
+    }
+}
