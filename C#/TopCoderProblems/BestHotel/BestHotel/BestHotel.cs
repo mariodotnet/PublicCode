6f47f39a38cc4fdb1a6b63ab4485e1cdc1e15e39
mariodotnet <contactomariodotnet@gmail.com>
2017-11-16 22:25:15 -0500

    muchs de ests cosas no se ocupan pero bueno...

---
+++ C#/TopCoderProblems/BestHotel/BestHotel/BestHotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestHotel
+{
+    class BestHotel
+    {
+        public int numberOfDisadvantageous(int[] price, int[] quality)
+        {
+            int disvantage = 0;
+
+           
+
+            return disvantage;
+        }
+    }
+}
---
+++ C#/TopCoderProblems/BestHotel/BestHotel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] precio = { 200, 300, 100, 500, 100, 100, 200 };
+            int[] calidad = { 7, 7, 4, 8, 4, 3, 6 };
+
+            var b = new BestHotel();
+            var r = b.numberOfDisadvantageous(precio, calidad);
+        }
+    }
+}
