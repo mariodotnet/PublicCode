5ddc007c75244cb91bf115ca8c15cf0e6b1a8478
mariodotnet <contactomariodotnet@gmail.com>
2017-11-22 00:48:22 -0500

    problema incompleto...

---
+++ C#/TopCoderProblems/Abacus/Abacus/Abacus.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abacus
+{
+    public class Abacus
+    {
+        public string[] add(string[] original, int val) {
+
+            // ponemos en resultado como la original, luego la modificaremos?
+            string[] r = original;
+
+
+            //valores que se agregan
+            for (int x = 0; x < val; x++)
+            {
+                bool acarreo = false; // si el valor es 'o' se acarre al siguiente...
+
+                // 6 lineas
+                for (int i = 5; i >= 0; i--) // valor mas a la derecha es el que importa.
+                {
+                    //10 circulos por linea
+                    for (int j = 9; j >= 0; j--) // idem
+                    {
+                        var arr = r[i].ToArray();
+
+                        if(arr[j] == 'o')
+                        {
+                            acarreo = true;
+                        }
+                        else
+                        {
+                            acarreo = false;
+                        }
+                    }
+                }
+            }
+            
+            return r;
+        }
+    }
+}
---
+++ C#/TopCoderProblems/Abacus/Abacus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abacus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var c = new Abacus();
+
+            string[] input = { "ooo---oooooo", "---ooooooooo", "---ooooooooo", "---ooooooooo", "oo---ooooooo", "---ooooooooo" };
+
+
+            c.add(input, 5);
+
+            //"ooo---oooooo", 
+            //"---ooooooooo", 
+            //"---ooooooooo", 
+            //"---ooooooooo", 
+            //"o---oooooooo", 
+            //"ooooo---oooo" }
+            //When we add 5 to the original, it is necessary to "carry" 1 to the next thread up.This shows the arithmetic 699979 + 5 = 699984
+        }
+    }
+}
