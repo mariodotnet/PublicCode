d1ff0bb495ef77f55c1fb9acdfd34db30260af35
mariodotnet <contactomariodotnet@gmail.com>
2017-11-22 14:41:07 -0500

    ejemplo linear search

---
+++ C#/AlgoritmosDeBusqueda/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //arreglo de datos
+            int[] datos = { 1, 3, 7, 4, 8, 9, 433, 5, 66, 99, 1, 2 };
+
+            //numero que queremos buscar
+            int numeroBusqueda = 99;
+
+            //ejecutamos el metodo
+            int indice = BusquedaLinear(datos, numeroBusqueda);
+
+            //imprimir indice
+            Console.WriteLine("Indice encontrado: {0}", indice);
+        }
+
+        //metodo de busqueda linear
+        private static int BusquedaLinear(int[] datos, int numeroBusqueda)
+        {
+            //iteramos todos los elementos
+            for (int i = 0; i < datos.Length; i++)
+            {
+                //si encontramos el valor en el arreglo, retornamos el indice
+                if(datos[i] == numeroBusqueda)
+                {
+                    return i;
+                }
+            }
+
+            //no se encontro el valor
+            return -1;
+        }
+    }
+}
